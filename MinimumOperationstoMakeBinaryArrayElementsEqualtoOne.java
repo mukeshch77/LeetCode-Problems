c5b7a8ea50f61178da363665f41f334daf7587d1
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-03-21 19:44:44 +0530

    commit

---
+++ MinimumOperationstoMakeBinaryArrayElementsEqualtoOne.java
@@ -0,0 +1,22 @@
+public class MinimumOperationstoMakeBinaryArrayElementsEqualtoOne {
+    public int minOperations(int[] nums) {
+        int n = nums.length;
+        int operations = 0;
+        
+        for (int i = 0; i <= n - 3; i++) {
+            if (nums[i] == 0) { 
+                operations++;
+    
+                nums[i] ^= 1;
+                nums[i + 1] ^= 1;
+                nums[i + 2] ^= 1;
+            }
+        }
+       
+        for (int num : nums) {
+            if (num == 0) return -1; 
+        }
+        
+        return operations;
+    }
+}
