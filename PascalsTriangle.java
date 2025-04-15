81d80d7c389f4af1f06d76c8741b96880da82b44
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-04-15 18:58:17 +0530

    commit

---
+++ PascalsTriangle.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        List<Integer> row = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            row.add(0, 1);
+
+            for (int j = 1; j < row.size() - 1; j++) {
+                row.set(j, row.get(j) + row.get(j + 1));
+            }
+
+            triangle.add(new ArrayList<>(row));
+        }
+
+        return triangle;
+    }
+}
