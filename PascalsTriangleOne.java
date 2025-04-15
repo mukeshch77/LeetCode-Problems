9e7511cbacf1b6af5558a08835338a1e6bacd313
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-04-15 18:59:01 +0530

    commit

--- PascalsTriangleOne.java
+++ PascalsTriangleOne.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class PascalsTriangle {
+public class PascalsTriangleOne {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> triangle = new ArrayList<>();
 
