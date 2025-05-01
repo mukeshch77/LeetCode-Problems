d278c5746d8e8ea1870fa488f53c5784ecc54bd1
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-05-01 17:25:41 +0530

    commit

---
+++ ReverseString.java
@@ -0,0 +1,12 @@
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int l = s.length - 1;
+        char temp;
+        for(int i = 0; i < s.length/2; i++, l--){
+            temp = s[i];
+            s[i] = s[l];
+            s[l] = temp;
+        
+        }
+    }
+}
