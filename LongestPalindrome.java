02096564141939a51036bd04d42fc49f00cb24ad
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-05-13 23:04:11 +0530

    commit

---
+++ LongestPalindrome.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+
+public class LongestPalindrome {
+    public int longestPalindrome(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int length = 0;
+        
+        for (char c : s.toCharArray()) {
+            if (set.contains(c)) {
+                set.remove(c);
+                length += 2;
+            } else {
+                set.add(c);
+            }
+        }
+        
+        if (!set.isEmpty()) {
+            length += 1;
+        }
+        
+        return length;
+    }
+}
