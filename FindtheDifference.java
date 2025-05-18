5e690417285ad993359de0904abf5f699ec59ba1
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-05-18 22:50:41 +0530

    commit

---
+++ FindtheDifference.java
@@ -0,0 +1,12 @@
+public class FindtheDifference {
+    public char findTheDifference(String s, String t) {
+        char res = 0;
+        for (char c : s.toCharArray()) {
+            res ^= c;
+        }
+        for (char c : t.toCharArray()) {
+            res ^= c;
+        }
+        return res;
+    }
+}
