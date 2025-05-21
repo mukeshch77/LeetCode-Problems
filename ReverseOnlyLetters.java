97ae65e2d9d703fc0334db7eff861d96a331e680
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-05-21 22:48:49 +0530

    commit

---
+++ ReverseOnlyLetters.java
@@ -0,0 +1,24 @@
+public class ReverseOnlyLetters {
+    public String reverseOnlyLetters(String s) {
+        char[] ch = s.toCharArray();
+        int l = 0, r = s.length() - 1;
+
+        while (l < r) {
+            while (l < r && !Character.isLetter(ch[l])) {
+                l++;
+            }
+            while (l < r && !Character.isLetter(ch[r])) {
+                r--;
+            }
+
+            char temp = ch[l];
+            ch[l] = ch[r];
+            ch[r] = temp;
+
+            l++;
+            r--;
+        }
+
+        return new String(ch);
+    }
+}
