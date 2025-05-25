7012280d7f07dc807b78f08e2b24cf704cbf21b0
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-05-25 22:52:11 +0530

    commit

---
+++ DetectCapital.java
@@ -0,0 +1,9 @@
+public class DetectCapital {
+
+    public boolean detectCapitalUse(String word) {
+        return word.equals(word.toUpperCase()) ||
+                word.equals(word.toLowerCase()) ||
+                word.substring(1).equals(word.substring(1).toLowerCase());
+    }
+    
+}
