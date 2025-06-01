fdad7583a8146ec5bc3dccd9a893d73e8809b88d
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-06-01 12:53:03 +0530

    commit

---
+++ RansomNote.java
@@ -0,0 +1,18 @@
+public class RansomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] l = new int[26]; 
+
+        for (char c : magazine.toCharArray()) {
+            l[c - 'a']++;
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            if (l[c - 'a'] == 0) {
+                return false;
+            }
+            l[c - 'a']--;
+        }
+
+        return true;
+    }
+}
