b2b38c70c9cb6cf83aedf4be4652cfc5b4700696
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-06-19 00:11:31 +0530

    commit

--- SearchinRotatedSortedArray.java
+++ SearchinRotatedSortedArray.java
@@ -1,25 +1,30 @@
 public class SearchinRotatedSortedArray {
     public int search(int[] nums, int target) {
-        int start = 0, end = nums.length - 1;
+        int start = 0;
+        int end = nums.length-1;
 
-        while (start <= end) {
+        while (start <= end){
             int mid = start + (end - start) / 2;
-            if (nums[mid] == target) {
+            if(target == nums[mid]){
                 return mid;
             }
+            // check left side is sorted
+            if(nums[start] <= nums[mid]){
+                // check ans is left side or not??
+                if(target >= nums[start] && target < nums[mid]){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
+            }else{// right side is sorted array
 
-            if (nums[start] <= nums[mid]) { // Left sorted
-                if (nums[start] <= target && target <= nums[mid]) {
-                    end = mid - 1;
-                } else {
+                // check ans is left side or not??
+                if(target > nums[mid] && target <= nums[end]){
                     start = mid + 1;
-                }
-            } else { // Right sorted
-                if (nums[mid] <= target && target <= nums[end]) {
-                    start = mid + 1;
-                } else {
+                }else{
                     end = mid - 1;
                 }
+                
             }
         }
         return -1;
