2bfb4c3965c5a442b7bfdb8608177dcad2c05dc4
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-06-18 15:22:22 +0530

    commit

--- MountainArray.java
+++ MountainArray.java
@@ -1,5 +1,19 @@
 public class MountainArray {
-    public boolean validMountainArray(int[] arr) {
+
+    public static void main(String[] args) {
+        int arr[] = {2, 4, 7, 9, 11, 18, 8, 3, 1};
+        int arr1[] = {2, 6, 9, 11, 18, 5, 1};
+        // boolean ans1 = validMountainArray(arr);
+        // System.out.println(ans1);
+
+        // int ans2 = peakIndexInMountainArray(arr);
+        // System.out.println(ans2);
+
+        int ans3 = findInMountainArray(arr1, 5);
+        System.out.println(ans3);
+    }
+
+    static boolean validMountainArray(int[] arr) {
         int index = 0;
         int n = arr.length;
 
@@ -42,5 +56,41 @@ public class MountainArray {
             }
         }
         return -1;
+    }
+
+    static int findInMountainArray(int arr[], int target){
+        int peak = peakIndexInMountainArray(arr);
+        System.out.println(peak);
+
+        // find in first half (increasing)
+        int start = 0;
+        int end = peak-1;
+        int mid = -1;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if(arr[mid] == target){
+                return mid;
+            }else if(arr[mid] < target){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+
+        // find in first half (increasing)
+        start = mid+1;
+        end = arr.length-1;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if(arr[mid] == target){
+                return mid;
+            }else if(arr[mid] < target){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return -1; 
+        
     }
 }
