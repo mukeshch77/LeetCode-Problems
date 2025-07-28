e440522b69b4debd0f678ca9d2232a5151828f89
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-07-28 23:08:50 +0530

    commit

---
+++ MinimumNumberofSwapstoMaketheStringBalanced.java
@@ -0,0 +1,19 @@
+public class MinimumNumberofSwapstoMaketheStringBalanced {
+    public int minSwaps(String s) {
+        int open = 0, close = 0;
+
+        for (int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if (ch == '[') {
+                open++;
+            }else{
+                if (open <= 0) {
+                    close++;
+                }else{
+                    open--;
+                }
+            }
+        }
+        return (open + 1) / 2;
+    }
+}
