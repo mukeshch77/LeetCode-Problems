dcdc88e3510bbc5854c373aa12765be0810eecf7
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-08-10 22:33:55 +0530

    commit

--- LargestRectangleinHistogram.java
+++ LargestRectangleinHistogram.java
@@ -1,4 +1,41 @@
 public class LargestRectangleinHistogram {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int stack[] = new int[n+1];
+        int index=-1;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i <= n; i++) {
+            int ele = (i == n) ? 0 : heights[i];
+            while (index != -1 && heights[stack[index]] > ele) {
+                int h = heights[stack[index--]];
+                int ps = (index == -1) ? -1 : stack[index];
+                int w = i - ps - 1;
+                max = Math.max(max, (h * w));
+            }
+            stack[++index] = i;
+        }
+        return (max == Integer.MIN_VALUE) ? 0 : max;
+    }
+
+    // public static int[] findNextSmaller(int arr[]){
+    //     int n = arr.length;
+    //     Stack<Integer> stack = new Stack<>();
+    //     int res[] = new int[n];
+    //     for (int i = n - 1; i >= 0; i--) {
+    //         while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+    //             stack.pop();
+    //         }
+    //         if (stack.isEmpty()) {
+    //             res[i] = n;
+    //         } else {
+    //             res[i] = stack.peek();
+    //         }
+    //         stack.push(i);
+    //     }
+    //     return res;
+    // }
+
+
     // public int largestRectangleArea(int[] heights) {
     //     Stack<Integer> stack = new Stack<>();
     //     int n = heights.length;
@@ -33,21 +70,4 @@ public class LargestRectangleinHistogram {
     //     return res;
     // }
 
-    // public static int[] findNextSmaller(int arr[]){
-    //     int n = arr.length;
-    //     Stack<Integer> stack = new Stack<>();
-    //     int res[] = new int[n];
-    //     for (int i = n - 1; i >= 0; i--) {
-    //         while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
-    //             stack.pop();
-    //         }
-    //         if (stack.isEmpty()) {
-    //             res[i] = n;
-    //         } else {
-    //             res[i] = stack.peek();
-    //         }
-    //         stack.push(i);
-    //     }
-    //     return res;
-    // }
 }
