8f09d7725f60b0af941a0f1d7893d0f82e694b1c
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-08-10 22:36:38 +0530

    commit

--- MaximalRectangle.java
+++ MaximalRectangle.java
@@ -1,47 +1,50 @@
 import java.util.Stack;
 
-public class MaximalRectangle {
-    public int maximalRectangle(char[][] matrix) {
-        if (matrix == null || matrix.length == 0) return 0;
-
-        int maxArea = 0;
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int[] heights = new int[cols];
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == '1') {
-                    heights[j]++;
-                } else {
-                    heights[j] = 0;
-                }
-            }
-            int area = largestRectangleInHistogram(heights);
-            maxArea = Math.max(maxArea, area);
-        }
-
-        return maxArea;
+public int maximalRectangle(char[][] matrix) {
+    // If there are no rows then return
+    if (matrix.length == 0) {
+        return 0;
     }
 
-    private int largestRectangleInHistogram(int[] heights) {
-        Stack<Integer> stack = new Stack<>();
-        int maxArea = 0;
-        int[] newHeights = new int[heights.length + 1];
-        for (int i = 0; i < heights.length; i++) {
-            newHeights[i] = heights[i];
-        }
-        newHeights[heights.length] = 0;
-
-        for (int i = 0; i < newHeights.length; i++) {
-            while (!stack.isEmpty() && newHeights[i] < newHeights[stack.peek()]) {
-                int height = newHeights[stack.pop()];
-                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
-                maxArea = Math.max(maxArea, height * width);
+    // Find the number of colums in a row.
+    int heights[] = new int[matrix[0].length];
+    int largest = 0;
+    for (int i = 0; i < matrix.length; i++) {
+        for (int j = 0; j < matrix[i].length; j++) {
+            int val = matrix[i][j] - '0'; // char to integer
+            if (val == 0) {
+                heights[j] = 0;
+            } else {
+                heights[j] += val;
             }
-            stack.push(i);
         }
-
-        return maxArea;
+        int maxArea = largestRectangleArea(heights);
+        if (largest < maxArea) {
+            largest = maxArea;
+        }
     }
+    return largest;
+}
+
+public int largestRectangleArea(int[] heights) {
+    int n = heights.length;
+    int stack[] = new int[n];
+    int index = -1;
+    int max = 0;
+    for (int i = 0; i < n; i++) {
+        while (index != -1 && heights[stack[index]] > heights[i]) {
+            int h = heights[stack[index--]];
+            int ps = (index == -1) ? -1 : stack[index];
+            int w = i - ps - 1;
+            max = Math.max(max, (h * w));
+        }
+        stack[++index] = i;
+    }
+    while (index != -1) {
+        int h = heights[stack[index--]];
+        int ps = (index == -1) ? -1 : stack[index];
+        int w = n - ps - 1;
+        max = Math.max(max, (h * w));
+    }
+    return max;
 }
