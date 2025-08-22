7e477f20a59167e74fe0cb7338ec552416045947
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-08-22 21:49:15 +0530

    commit

--- ConcatenationofArray.java
+++ ConcatenationofArray.java
@@ -3,17 +3,17 @@ public class ConcatenationofArray {
         int n = nums.length;
         int[] ans = new int[2 * n];
         
-        // int i = 0;
-        // while (i < n) {
-        //     ans[i] = nums[i];
-        //     ans[i + n] = nums[i];
-        //     i++;
-        // }
-
-        for (int i = 0; i < n; i++) {
+        int i = 0;
+        while (i < n) {
             ans[i] = nums[i];
             ans[i + n] = nums[i];
+            i++;
         }
+
+        // for (int i = 0; i < n; i++) {
+        //     ans[i] = nums[i];
+        //     ans[i + n] = nums[i];
+        // }
        
         // for (int i = 0; i < n; i++) {
         //     ans[i + n] = nums[i];
